dcb88154ed8a8ddc5b635e76b0670bfacc01e864
LKing-220 <131147494+LKing-220@users.noreply.github.com>
2023-04-27 22:44:35 +0800

    Delete CAT directory

--- CAT/js/none.js
+++
@@ -1,7 +0,0 @@
-function none() {
-    main.style.display = 'none';
-    essay.style.display = 'none';
-    modifyInformation.style.display = 'none';
-    userinfo.style.display = 'none';
-    addEssay.style.display = 'none';
-}
